8398be53826bbe4fd3ccac0433f57e1b222208b2
Clau050994 <111773719+Clau050994@users.noreply.github.com>
2023-10-30 22:36:26 +0000

    i added new features to the petition form with input validation

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const addSignature = () => {
   const hometown = document.getElementById('hometown').value; 
   const email = document.getElementById('email').value; 
 
+
   
   // Create a new paragraph element for the new signature
   const newSignature = document.createElement('p');
@@ -47,8 +48,57 @@ const addSignature = () => {
   
 }
 
-// Add a click event listener to the sign now button
-signNowButton.addEventListener('click', addSignature);
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button
+    const createAccountButton = document.getElementById("create-account-button");
+
+    // Add an event listener to the button
+    createAccountButton.addEventListener("click", function() {
+        // Redirect to the registration page when the button is clicked
+        window.location.href = "registration.html";
+    });
+});
+
+
+// Validation form for the petition
+const validateForm = () => {
+  let containsErrors = false;
+  const petitionInputs = document.getElementById("sign-petition").elements;
+
+  for (let i = 0; i < petitionInputs.length; i++) 
+  {
+    const trimmedValue = petitionInputs[i].value.trim();
+    console.log(`Checking input: ${petitionInputs[i].name},                 trimmed value: '${trimmedValue}', length:           ${trimmedValue.length}`);
+    if (trimmedValue.length <= 2) {
+      console.log(`Error found in input: ${petitionInputs[i].name}`);
+      petitionInputs[i].classList.add('error');
+      containsErrors = true;
+    } else {
+      petitionInputs[i].classList.remove('error');
+    }
+  }
+
+  // Specific validation for email
+  const emailInput = document.getElementById('email');
+  const emailValue = emailInput.value;
+  if (!emailValue.includes('.com')) {
+      containsErrors = true;
+      emailInput.classList.add('error');
+  } else {
+      emailInput.classList.remove('error');
+  }
+
+  if (!containsErrors) {
+    console.log("No errors found. Adding signature.");
+    addSignature();
+    for (let i = 0; i < petitionInputs.length; i++) {
+      petitionInputs[i].value = "";
+    }
+  }
+  
+ 
+};
 
 
 
+signNowButton.addEventListener('click', validateForm);
